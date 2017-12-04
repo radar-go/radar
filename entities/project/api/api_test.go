5e9964be711d81e24a69e8351cc461147d230eaa
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-03 21:46:28 -0600

    Add tests to the project entity.

---
+++ entities/project/api/api_test.go
@@ -0,0 +1,32 @@
+// Package api defines the protocol for the project entity.
+package api
+
+/* Copyright (C) 2017 Radar team (see AUTHORS)
+
+   This file is part of radar.
+
+   radar is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   radar is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with radar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import (
+	"testing"
+)
+
+func TestProject(t *testing.T) {
+	project := New("Ritho")
+
+	if project.Name() != "Ritho" {
+		t.Errorf("Error creating a new member, expected Ritho, got %s", project.Name())
+	}
+}
