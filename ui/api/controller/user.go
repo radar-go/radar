d821300ad2f1f3f246db3bf5675a80bba7ed6359
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-10 06:56:11 -0600

    Implement the User Registration use case.

--- ui/api/controller/user.go
+++ ui/api/controller/user.go
@@ -19,12 +19,79 @@ package controller
 */
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
 	"github.com/valyala/fasthttp"
+
+	"github.com/radar-go/radar/casesprovider"
 )
 
 func (c *Controller) userRegistration(ctx *fasthttp.RequestCtx) {
 	logPath(ctx.Path())
-	ctx.SetStatusCode(fasthttp.StatusOK)
 	ctx.SetContentType("application/json; charset=utf-8")
-	ctx.SetBodyString(`{}`)
+
+	ct := ctx.Request.Header.Peek("Content-Type")
+	if !bytes.Contains(ct, []byte("application/json")) {
+		badRequest(ctx, "Expected json format for the request.")
+		return
+	}
+
+	body := ctx.PostBody()
+	if len(body) == 0 {
+		badRequest(ctx, "Missing the parameters for the user registration.")
+		return
+	}
+
+	var params struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.Unmarshal(body, &params)
+	if err != nil {
+		badRequest(ctx, "Error obtaining the user params")
+		return
+	}
+
+	uc, err := casesprovider.GetUseCase("UserRegister")
+	if err != nil {
+		internalServerError(ctx, fmt.Sprintf("Error obtaining the use case: %s.", err))
+		return
+	}
+
+	err = uc.AddParam("name", params.Name)
+	if err != nil {
+		internalServerError(ctx, fmt.Sprintf("Error adding the ad param: %s.", err))
+		return
+	}
+
+	err = uc.AddParam("email", params.Email)
+	if err != nil {
+		internalServerError(ctx, fmt.Sprintf("Error adding the ad param: %s.", err))
+		return
+	}
+
+	err = uc.AddParam("password", params.Password)
+	if err != nil {
+		internalServerError(ctx, fmt.Sprintf("Error adding the ad param: %s.", err))
+		return
+	}
+
+	res, err := uc.Run()
+	if err != nil {
+		badRequest(ctx, fmt.Sprintf("Error registering the user: %s.", err))
+		return
+	}
+
+	result, err := res.Bytes()
+	if err != nil {
+		internalServerError(ctx, fmt.Sprintf("Error generating the result: %s.", err))
+		return
+	}
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBody(result)
 }
