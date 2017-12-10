385de8b2e8d9629f1233f1967044dc95fd2ab879
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-09 18:01:20 -0600

    Add a config package for the common configurations.

---
+++ config/config.go
@@ -0,0 +1,32 @@
+// Package config implements the common general configurations.
+package config
+
+/* Copyright (C) 2017 Radar team (see AUTHORS)
+
+   This file is part of radar.
+
+   radar is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   radar is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with radar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Config structure to store the general configurations.
+type Config struct {
+	APIPort int
+}
+
+// New creates and returns a new Config object.
+func New() *Config {
+	return &Config{
+		APIPort: 10000,
+	}
+}
