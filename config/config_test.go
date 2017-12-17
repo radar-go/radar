1487b72fc32059882c4c374fbb28375f865452a1
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-16 20:51:44 -0600

    Add config tests.

---
+++ config/config_test.go
@@ -0,0 +1,30 @@
+package config
+
+/* Copyright (C) 2017 Radar team (see AUTHORS)
+
+   This file is part of radar.
+
+   radar is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   radar is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with radar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import (
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	cfg := New()
+	if cfg.APIPort != 10000 {
+		t.Errorf("Expected 10000, got %d", cfg.APIPort)
+	}
+}
