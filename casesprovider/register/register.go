1ad90dceae600c8b2cf93570c4e5fd96c5bf5faf
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-19 01:47:48 -0600

    Refactor usecase and registration use cases to make it more generic.

--- casesprovider/register/register.go
+++ casesprovider/register/register.go
@@ -20,99 +20,50 @@ package register
 */
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"github.com/radar-go/radar/casesprovider/errors"
 	"github.com/radar-go/radar/casesprovider/usecase"
 )
 
 // UseCase for the user registration.
 type UseCase struct {
 	usecase.UseCase
-	userName string
-	email    string
-	password string
 }
 
 // Result stores the result of the user registration.
 type Result struct {
 	usecase.Result
-	UserID int `json:"id,omitempty"`
 }
 
 // New creates and returns a new register use case object.
 func New() *UseCase {
 	uc := &UseCase{}
 	uc.Name = "UserRegister"
+	uc.Params = make(map[string]interface{})
+	uc.Params["name"] = ""
+	uc.Params["email"] = ""
+	uc.Params["password"] = ""
 
 	return uc
 }
 
-// AddParam adds a new ad param to the use case.
-func (uc *UseCase) AddParam(name string, value interface{}) error {
-	switch name {
-	case "name":
-		name, ok := value.(string)
-		if !ok {
-			return errors.ErrParamType
-		} else if len(name) == 0 {
-			return errors.ErrParamEmpty
-		}
-
-		uc.userName = name
-	case "email":
-		email, ok := value.(string)
-		if !ok {
-			return errors.ErrParamType
-		} else if len(email) == 0 {
-			return errors.ErrParamEmpty
-		}
-
-		uc.email = email
-	case "password":
-		passwd, ok := value.(string)
-		if !ok {
-			return errors.ErrParamType
-		} else if len(passwd) == 0 {
-			return errors.ErrParamEmpty
-		}
-
-		uc.password = passwd
-	default:
-		return errors.ErrParamUnknown
-	}
-
-	return nil
-}
-
 // Run tries to register a new user in the system.
 func (uc *UseCase) Run() (usecase.ResultPrinter, error) {
-	res := &Result{}
+	res := usecase.NewResult()
 
-	userID, err := uc.Datastore.UserRegistration(uc.userName, uc.email, uc.password)
+	userID, err := uc.Datastore.UserRegistration(
+		uc.Params["name"].(string),
+		uc.Params["email"].(string),
+		uc.Params["password"].(string),
+	)
+
 	if err != nil {
-		res.Message = "Error registering the user"
-		res.Error = fmt.Sprintf("%s", err)
+		res.Res["result"] = "Error registering the user"
+		res.Res["error"] = fmt.Sprintf("%s", err)
 	} else {
-		res.Message = "User registered successfully"
-		res.UserID = userID
+		res.Res["result"] = "User registered successfully"
+		res.Res["id"] = userID
 	}
 
 	return res, err
-}
-
-// Bytes returns the use case result in string format.
-func (r *Result) String() (string, error) {
-	res, err := json.Marshal(r)
-	if err != nil {
-		return "{}", err
-	}
-
-	return fmt.Sprintf("%s", res), err
-}
-
-// Bytes returns the use case result in []bytes format.
-func (r *Result) Bytes() ([]byte, error) {
-	return json.Marshal(r)
 }
