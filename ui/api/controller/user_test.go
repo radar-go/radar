d0dafb861445fe4640b2640331b34c6bf9d84c2e
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-12-31 22:07:31 -0600

    Add tests for /login endpoint.
    Closes #6

--- ui/api/controller/user_test.go
+++ ui/api/controller/user_test.go
@@ -42,7 +42,7 @@ func TestUserControllerFormatError(t *testing.T) {
 	}
 }
 
-func TestUserControllerMissingParamsError(t *testing.T) {
+func TestUserControllerBodyError(t *testing.T) {
 	ctx := &fasthttp.RequestCtx{}
 	ctx.Request.Header.Set("Content-Type", "application/json")
 	ctx.Request.Header.SetRequestURI("/register")
@@ -58,104 +58,114 @@ func TestUserControllerMissingParamsError(t *testing.T) {
 	}
 }
 
-func TestUserControllerInvalidEmailFormatError(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"username": "ritho", "name": "ritho", "email": "ritho", "password": "ritho"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 400 {
-		t.Errorf("Expected 400, Got %d", ctx.Response.StatusCode())
+func TestPostHandler(t *testing.T) {
+	testCases := []struct {
+		name     string
+		endpoint string
+		input    string
+		code     int
+		output   string
+	}{
+		{
+			"RegisterInvalidEmailFormatError",
+			"/register",
+			`{"username": "ritho", "name": "ritho", "email": "ritho", "password": "ritho"}`,
+			400,
+			`Error validating the email: invalid format`,
+		},
+		{
+			"RegisterUnresolvedHostError",
+			"/register",
+			`{"name": "ritho", "email": "palvarez@invalid.es", "password": "ritho"}`,
+			400,
+			`Error validating the email: unresolvable host`,
+		},
+		{
+			"RegisterUsernameShort",
+			"/register",
+			`{"username": "rit", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`,
+			400,
+			`Error registering the user: Username too short`,
+		},
+		{
+			"RegisterPasswordShort",
+			"/register",
+			`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "1234"}`,
+			400,
+			`Error registering the user: Password too short`,
+		},
+		{
+			"RegisterSuccess",
+			"/register",
+			`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`,
+			200,
+			`"result":"User registered successfully"`,
+		},
+		{
+			"RegisterDuplicateUser",
+			"/register",
+			`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`,
+			400,
+			`palvarez@ritho.net: User already exists`,
+		},
+		{
+			"LoginUnknownParam",
+			"/login",
+			`{"login": "ritho", "passwerd": "ritho"}`,
+			500,
+			`Error adding the param passwerd, key doesn't exists: Unknown parameter for the use case`,
+		},
+		{
+			"LoginUsernameShort",
+			"/login",
+			`{"login": "rit", "password": "ritho"}`,
+			400,
+			`Username too short`,
+		},
+		{
+			"LoginPasswordShort",
+			"/login",
+			`{"login": "ritho", "password": "rit"}`,
+			400,
+			`Password too short`,
+		},
+		{
+			"LoginWrongPassword",
+			"/login",
+			`{"login": "ritho", "password": "rithoo"}`,
+			400,
+			`Password missmatch`,
+		},
+		{
+			"LoginSuccess",
+			"/login",
+			`{"login": "ritho", "password": "ritho"}`,
+			200,
+			`"result":"User login successfully"`,
+		},
+		{
+			"LoginAlreadyLogin",
+			"/login",
+			`{"login": "ritho", "password": "ritho"}`,
+			400,
+			`ritho: User already logged in`,
+		},
 	}
 
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"error":"Error registering the user: Error validating the email: invalid format."}`)) {
-		t.Errorf(`Expected {"error":"Error registering the user: Error validating the email: invalid format."}, Got %s`,
-			ctx.Response.Body())
-	}
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := &fasthttp.RequestCtx{}
+			ctx.Request.Header.Set("Content-Type", "application/json")
+			ctx.Request.Header.SetRequestURI(tc.endpoint)
+			ctx.Request.SetBody([]byte(tc.input))
+			c.postHandler(ctx)
+			if ctx.Response.StatusCode() != tc.code {
+				t.Errorf("Expected %d, Got %d", tc.code, ctx.Response.StatusCode())
+			}
 
-func TestUserControllerUnresolvedHostError(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"name": "ritho", "email": "palvarez@invalid.es", "password": "ritho"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 400 {
-		t.Errorf("Expected 400, Got %d", ctx.Response.StatusCode())
-	}
-
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"error":"Error registering the user: Error validating the email: unresolvable host."}`)) {
-		t.Errorf(`Expected {"error":"Error registering the user: Error validating the email: unresolvable host."}, Got %s`,
-			ctx.Response.Body())
-	}
-}
-
-func TestUserControllerUsernameShort(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"username": "rit", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 400 {
-		t.Errorf("Expected 400, Got %d", ctx.Response.StatusCode())
-	}
-
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"error":"Error registering the user: Username too short."}`)) {
-		t.Errorf(`Expected {"error":"Error registering the user: Username too short."}, Got %s`,
-			ctx.Response.Body())
-	}
-}
-
-func TestUserControllerPasswordShort(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "1234"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 400 {
-		t.Errorf("Expected 400, Got %d", ctx.Response.StatusCode())
-	}
-
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"error":"Error registering the user: Password too short."}`)) {
-		t.Errorf(`Expected {"error":"Error registering the user: Password too short."}, Got %s`,
-			ctx.Response.Body())
-	}
-}
-
-func TestUserControllerSuccess(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 200 {
-		t.Errorf("Expected 200, Got %d", ctx.Response.StatusCode())
-	}
-
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"id":1,"result":"User registered successfully"}`)) {
-		t.Errorf(`Expected {"id":1,"result":"User registered successfully"}, Got %s`,
-			ctx.Response.Body())
-	}
-}
-
-func TestUserControllerDuplicateUser(t *testing.T) {
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.Set("Content-Type", "application/json")
-	ctx.Request.Header.SetRequestURI("/register")
-	ctx.Request.SetBody([]byte(`{"username": "ritho", "name": "ritho", "email": "palvarez@ritho.net", "password": "ritho"}`))
-
-	c.postHandler(ctx)
-	if ctx.Response.StatusCode() != 400 {
-		t.Errorf("Expected 400, Got %d", ctx.Response.StatusCode())
-	}
-
-	if !bytes.Equal(ctx.Response.Body(), []byte(`{"error":"Error registering the user: palvarez@ritho.net: User already exists."}`)) {
-		t.Errorf(`Expected {"error":"Error registering the user: palvarez@ritho.net: User already exists."}, Got %s`,
-			ctx.Response.Body())
+			if !bytes.Contains(ctx.Response.Body(), []byte(tc.output)) {
+				t.Errorf(`Expected %s, Got %s`, tc.output, ctx.Response.Body())
+			}
+		})
 	}
 }
