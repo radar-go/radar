7a0d29fedfa3472902c53c7855a739ee53399241
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2018-02-10 20:17:02 -0600

    Complete a first account activation test.

--- casesprovider/cases/account/activate/activate_test.go
+++ casesprovider/cases/account/activate/activate_test.go
@@ -19,10 +19,21 @@ package activate
 */
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/radar-go/radar/datastore"
 )
+
+func addParam(t *testing.T, uc *UseCase, param string, value interface{}) {
+	t.Helper()
+
+	err := uc.AddParam(param, value)
+	if err != nil {
+		t.Errorf("Unknown param %s", param)
+	}
+}
 
 func TestAccountActivation(t *testing.T) {
 	uc := New()
@@ -30,5 +41,62 @@ func TestAccountActivation(t *testing.T) {
 		t.Errorf("Expected AccountActivate, Got %s", uc.Name)
 	}
 
+	newUC := uc.New()
+	if newUC.GetName() != "AccountActivate" {
+		t.Errorf("Expected AccountActivate, Got %s", newUC.GetName())
+	}
+
 	uc.SetDatastore(datastore.New())
+	addParam(t, uc, "id", 1)
+	addParam(t, uc, "token", "00000000-0000-0000-0000-000000000000")
+
+	_, err := uc.Run()
+	if err == nil {
+		t.Error("Expected error running the use case")
+	} else if fmt.Sprint(err) != "00000000-0000-0000-0000-000000000000: User not logged in" {
+		t.Errorf("Expected '00000000-0000-0000-0000-000000000000: User not logged in', got %s", err)
+	}
+
+	err = uc.AddParam("tokens", "12345")
+	if !strings.Contains(fmt.Sprintf("%v", err), "Error adding the param tokens") {
+		t.Errorf("Expected error to contain 'Error adding the param tokens', Got '%v'",
+			err)
+	}
+
+	id, err := uc.Datastore.AccountRegistration("ritho", "ritho", "palvarez@ritho.net",
+		"12345")
+	if err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	addParam(t, uc, "id", id)
+	_, err = uc.Run()
+	if err == nil {
+		t.Error("Expected error running the use case")
+	} else if fmt.Sprint(err) != "00000000-0000-0000-0000-000000000000: User not logged in" {
+		t.Errorf("Expected '00000000-0000-0000-0000-000000000000: User not logged in', got %s", err)
+	}
+
+	err = uc.Datastore.AddSession("00000000-0000-0000-0000-000000000000", "ritho")
+	if err != nil {
+		t.Errorf("Unexpected error %+v", err)
+	}
+
+	res, err := uc.Run()
+	if err != nil {
+		t.Errorf("Unexpected error %+v", err)
+	}
+
+	plainResult, err := res.String()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if !strings.Contains(plainResult, fmt.Sprintf(`"id":%d`, id)) {
+		t.Errorf("Expected id %d in result '%s'", id, plainResult)
+	}
+
+	if !strings.Contains(plainResult, `Account activated successfully`) {
+		t.Errorf("Expected to activate the account successfully, Got '%s'", plainResult)
+	}
 }
