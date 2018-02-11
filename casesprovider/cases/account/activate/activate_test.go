ee35000d14625a821a7dd6947ba750fcbd3a5225
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2018-02-10 19:57:12 -0600

    Add endpoints for activate and deactivate and add the first tests for activate and deactivate use cases.

---
+++ casesprovider/cases/account/activate/activate_test.go
@@ -0,0 +1,34 @@
+package activate
+
+/* Copyright (C) 2018 Radar team (see AUTHORS)
+
+   This file is part of radar.
+
+   radar is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   radar is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with radar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import (
+	"testing"
+
+	"github.com/radar-go/radar/datastore"
+)
+
+func TestAccountActivation(t *testing.T) {
+	uc := New()
+	if uc.Name != "AccountActivate" {
+		t.Errorf("Expected AccountActivate, Got %s", uc.Name)
+	}
+
+	uc.SetDatastore(datastore.New())
+}
